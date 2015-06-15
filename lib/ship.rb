8e74a6203a9c8c727189f133ee6aa3dcedc11cb7
Salman <salman.karim@hotmail.com>
2015-06-15 22:53:29 +0100

    creates Ship#place_vertical and Ship#place_horizontal

--- lib/ship.rb
+++ lib/ship.rb
@@ -3,9 +3,18 @@ class Ship
   attr_reader :ships, :grid, :alphabet
 
   def initialize
-    @grid = [['x','x'],
-             ['x','x']]
-    @ships = {'destroyer' => 2, 'battleship' => 4} #Why can't we use the symbol creator?
+    @grid = [['x','x','x','x','x','x','x','x','x','x'],
+             ['x','x','x','x','x','x','x','x','x','x'],
+             ['x','x','x','x','x','x','x','x','x','x'],
+             ['x','x','x','x','x','x','x','x','x','x'],
+             ['x','x','x','x','x','x','x','x','x','x'],
+             ['x','x','x','x','x','x','x','x','x','x'],
+             ['x','x','x','x','x','x','x','x','x','x'],
+             ['x','x','x','x','x','x','x','x','x','x'],
+             ['x','x','x','x','x','x','x','x','x','x'],
+             ['x','x','x','x','x','x','x','x','x','x'],]
+
+    @ships = {'destroyer' => 2, 'cruiser' => 3, 'submarine' => 3, 'battleship' => 4, 'aircraft carrier' => 5} #Why can't we use the symbol creator?
     @alphabet = ('a'..'z').to_a
 
   end
@@ -20,11 +29,13 @@ class Ship
   def place_vertical(ship,y,x)
     # fail 'Ship already placed there!' if @board[x-1..ships[ship]+1].any? {|element| element!='w'}
     # fail 'Cannot place ship outside board!' if ((x-1)+ships[ship])>@board.length
+    combine_y_x(ship,y,x)
+    @grid
   end
 
 
   def convert_y(ship,y)
-    y_array = (y-1..ships[ship]-1).to_a
+    y_array = (y-1...y+ships[ship]-1).to_a
     # y_array_of_arrays = y_array.each {|y| y.to_a}
   end
 
@@ -34,7 +45,7 @@ class Ship
     # x_array_of_arrays = x_array {|x| x.to_a}
   end
 
-  def combine(ship,y,x)
+  def combine_y_x(ship,y,x)
      z = convert_y(ship,y).zip(convert_x(ship,x))
      z.each { |y,x| @grid[y][x] = ship[0] }
   end
