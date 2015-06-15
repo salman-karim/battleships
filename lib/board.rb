47a75a2ed7f2544dff82d4a4738e833bc4b8e564
Robert Morgan <robbie.morgan@gmail.com>
2015-06-15 17:57:29 +0100

    restricts ship placement

--- lib/board.rb
+++ lib/board.rb
@@ -4,23 +4,26 @@ class Board
 
 
 
-  attr_reader :ships
+  attr_reader :ships, :board
 
   def initialize
     @board = ['w','w','w','w','w','w','w','w','w','w']
-    @ships = {'destroyer' => 3, 'battleship' => 4}
+    @ships = {'destroyer' => 3, 'battleship' => 4} #Why can't we use the symbol creator?
   end
 
   def place(ship, point)
-     @board[point-1..ships[ship]+1] = create_array(ship, point)
+    fail 'Ship already placed there!' if @board[point-1..ships[ship]+1].any? {|x| x!='w'}
+    fail 'Cannot place ship outside board!' if ((point-1)+ships[ship])>@board.length
+     @board[point-1..ships[ship]+1] = create_array(ship)
+     @board
   end
 
-  def create_array(ship, point)
+  def create_array(ship)
       (ship[0]*ships[ship]).split('')
   end
 
-  def strike(position)
-    @board.include?(position) ? 'HIT!' : 'MISS!'
+  def strike(target)
+    @board[target]=='w' ? 'MISS!' : 'HIT!'
   end
 
 end
--- spec/board_spec.rb
+++ spec/board_spec.rb
@@ -3,23 +3,35 @@ require 'ship'
 
 describe Board do
 
+  describe 'place' do
+    it 'Places the ship on the board at a specific point' do
+      subject.place('destroyer',2)
+      expect(subject.board[1..3]).to eq ['d','d','d']
+    end
+
+    it 'Doesn\'t allow ships to overlap' do
+      subject.place('destroyer',2)
+      expect{subject.place('battleship',3)}.to raise_error 'Ship already placed there!'
+    end
+
+    it 'Doesn\'t allow ships to be placed outside the board' do
+      expect{subject.place('battleship',10)}.to raise_error 'Cannot place ship outside board!'
+    end
+  end
+
   describe 'strike' do
     it 'can hit a ship placed on the board' do
-      ship = double :ship, position: 'A4'
-      subject.place ship
-      expect(subject.strike(ship.position)).to eq 'HIT!'
+      subject.place('destroyer',2)
+      expect(subject.strike(3)).to eq 'HIT!'
     end
 
     it 'returns a miss if ship is not hit' do
-      expect(subject.strike("A1")).to eq 'MISS!'
+      subject.place('destroyer',2)
+      expect(subject.strike(7)).to eq 'MISS!'
     end
   end
 
-  describe 'place ship' do
-    it 'places a ship on the board' do
-      ship = double :ship, position: 'A4'
-      subject.place ship
-      expect(subject.board).to include("A4")
-    end
-  end
+
+
+
 end
